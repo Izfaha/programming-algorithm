8c16477804f7615ad6874c3fd8cd19ff71abc87d
Izfaha <rasberry006@gmail.com>
2025-03-18 13:34:54 +0700

    .

---
+++ c++-algorithm/tugas.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void printArrayBeforeSorting(int B[], int sizeArray){
+    for(int i = 0; i < sizeArray; ++i){
+        cout << B[i] << " ";
+    }
+    cout << endl;
+}
+
+void sortArray(int B[], int sizeArray){
+    bool ascendingOrDescending = B[0] < B[sizeArray-1];
+
+    if(ascendingOrDescending == B[0] < B[sizeArray-1]){
+        for(int i = 0; i < sizeArray; ++i){
+            int minIndex = i;
+    
+            for(int j = minIndex + 1; j < sizeArray; ++j){
+                if(ascendingOrDescending ? (B[j]<B[minIndex]) : (B[j]>B[minIndex])){
+                    minIndex = j;
+                }
+            }
+            swap(B[i], B[minIndex]);
+        }
+    }
+}
+
+int main(){
+    int B[]={15, 12, 20, 50, 100};
+    int sizeArray = sizeof(B)/sizeof(B[0]);
+    B[4] = 1; // Rubah array DISINI
+
+    cout << "Array before sorting : ";
+    printArrayBeforeSorting(B, sizeArray);
+    
+    sortArray(B, sizeArray);
+
+    cout << "Array after sorting : ";
+    sortArray(B, sizeArray);
+    printArrayBeforeSorting(B, sizeArray);
+
+    return 0;
+}
